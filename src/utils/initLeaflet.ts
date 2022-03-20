86b9e9c87f595bea62fcd3510e10b52d2a20af10
tkyk.niimura <tkyk.niimura@gmai.com>
2022-03-20 09:43:57 +0900

    1st sample

---
+++ src/utils/initLeaflet.ts
@@ -0,0 +1,12 @@
+// add icons
+import Leaflet from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+// marker setting
+let DefaultIcon = Leaflet.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconAnchor: [12, 41], // アイコンのとがった位置をクリックした位置に合わせるためのオフセット
+});
+Leaflet.Marker.prototype.options.icon = DefaultIcon;
